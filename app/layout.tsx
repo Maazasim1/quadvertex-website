6ac5f3f399af61c4154b41f499a51e6b8964fc0c
maaz <mazzasimq1@gmail.com>
2024-11-18 11:49:56 +0500

    added dynamic favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,20 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "QuadVertex",
   description: "QuadVertex | The leading IT, Software, Web, App, and Emerging Technologies Services & Solutions | Enabling & Transforming Digitally Fortune 500 Clients",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/icon.ico',
+        href: '/icon.ico',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/icon-dark.ico',
+        href: '/icon-dark.ico',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
